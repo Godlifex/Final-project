75a832daedb34179d31c2b4668874cb53c89f88d
Godlifex <prediger.denas@gmail.com>
2023-09-30 01:20:38 +0300

    code

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable no-unused-vars */
+import {React} from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const initialValues = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+  };
+
+  const validationSchema = Yup.object({
+    firstName: Yup.string().required('First name is required'),
+    lastName: Yup.string().required('Last name is required'),
+    email: Yup.string().email('Invalid email address').required('Email is required'),
+    password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
+  });
+
+  const onSubmit = (values) => {
+    // Handle form submission, e.g., send data to the server
+    console.log('Form data:', values);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="registration-form">
+      <h2>Registration Form</h2>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+      >
+        <Form>
+          <div className="form-table">
+            <div className="form-row">
+              <label htmlFor="firstName">First Name</label>
+              <Field type="text" id="firstName" name="firstName" />
+              <ErrorMessage name="firstName" component="div" className="error" />
+            </div>
+            <div className="form-row">
+              <label htmlFor="lastName">Last Name</label>
+              <Field type="text" id="lastName" name="lastName" />
+              <ErrorMessage name="lastName" component="div" className="error" />
+            </div>
+            <div className="form-row">
+              <label htmlFor="email">Email</label>
+              <Field type="email" id="email" name="email" />
+              <ErrorMessage name="email" component="div" className="error" />
+            </div>
+            <div className="form-row">
+              <label htmlFor="password">Password</label>
+              <Field type="password" id="password" name="password" />
+              <ErrorMessage name="password" component="div" className="error" />
+            </div>
+          </div>
+          <button type="submit">Register</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
 
 export default App
